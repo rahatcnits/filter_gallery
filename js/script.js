0c5cd2b0f556e753f7aa50cbed658eeafce80460
rahatcnits <rahatcnits@gmail.com>
2023-08-24 23:08:51 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const filterButtons = document.querySelectorAll(".filter_button button");
+const filterableCards = document.querySelectorAll(".filterable_cards .card");
+
+const filterCards = (e) => {
+  document.querySelector(".active").classList.remove("active");
+  e.target.classList.add("active");
+
+  filterableCards.forEach((card) => {
+    card.classList.add("hide");
+
+    if (
+      card.dataset.name === e.target.dataset.name ||
+      e.target.dataset.name === "all"
+    ) {
+      card.classList.remove("hide");
+    }
+  });
+};
+
+filterButtons.forEach((btn) => btn.addEventListener("click", filterCards));
